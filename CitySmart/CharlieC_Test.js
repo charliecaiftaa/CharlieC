73e82d0992dc3023dc3606652b367b58753862d6
charliecaiftaa <charlie.cai@g.feitianacademy.org>
2018-01-30 16:12:10 -0500

    ...

--- CitySmart/CharlieC_Test.js
+++ CitySmart/CharlieC_Test.js
@@ -4,3 +4,7 @@ ellipse(41.452035, -74.438758, 500, 500)
 if (rgb(255, 0, 0)) {
     {wwd.layers[Layer_Number].enabled = true; layerManager.goToAnimator.goTo(new WorldWind.Position(41.452035, -74.438758, 2500));}
 }
+
+/*
+    In CSS code, for the placemark, you need to add (font-size: 30px;). This enlargens the thing.
+ */
